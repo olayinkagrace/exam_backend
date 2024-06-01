fc7536dee73be02102eed3b9346ee1c3b3e6a35d
olayinkagrace <olayinka4grace@gmail.com>
2024-06-01 12:03:30 +0100

    commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path}`);
   next();
 });
 
@@ -19,13 +18,11 @@ const uri = process.env.MONGODB_URI;
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('Connected to MongoDB');
     app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
     });
   })
   .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   });
 
 const userSchema = new mongoose.Schema({
@@ -35,65 +32,69 @@ const userSchema = new mongoose.Schema({
   score: { type: Number, default: 0 },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
-app.post('/signup', async (req, res) => {
+app.post("/signup", async (req, res) => {
   const { name, email, password } = req.body;
   try {
     const existingUserByName = await User.findOne({ name });
     const existingUserByEmail = await User.findOne({ email });
     if (existingUserByName || existingUserByEmail) {
-      return res.status(400).json({ error: 'User already exists' });
+      return res.status(400).json({ error: "User already exists" });
     }
     const newUser = new User({ name, email, password });
     await newUser.save();
-    res.status(200).json({ message: 'Signup successful' });
+    res.status(200).json({ message: "Signup successful" });
   } catch (error) {
-    console.error('Error during signup:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error("Error during signup:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
     if (user && user.password === password) {
       if (user.score === 0) {
-        return res.status(200).json({ message: 'Login successful' });
+        return res.status(200).json({ message: "Login successful" });
       } else if (user.score !== 0) {
-        return res.status(403).json({ error: 'You have already taken the test' });
+        return res
+          .status(403)
+          .json({ error: "You have already taken the test" });
       }
     } else {
-      return res.status(400).json({ error: 'Invalid credentials' });
+      return res.status(400).json({ error: "Invalid credentials" });
     }
   } catch (error) {
-    console.error('Error during login:', error);
-    return res.status(500).json({ error: 'Internal server error' });
+    console.error("Error during login:", error);
+    return res.status(500).json({ error: "Internal server error" });
   }
 });
 
-
-app.post('/submit', async (req, res) => {
+app.post("/submit", async (req, res) => {
   const { email, score } = req.body;
   try {
     const user = await User.findOne({ email });
     if (user) {
       user.score = score;
       await user.save();
-      res.status(200).json({ message: 'Score submitted' });
+      res.status(200).json({ message: "Score submitted" });
     } else {
-      res.status(400).json({ error: 'User not found' });
+      res.status(400).json({ error: "User not found" });
     }
   } catch (error) {
-    console.error('Error during score submission:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error("Error during score submission:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
-app.get('/users', async (req, res) => {
+app.get("/users", async (req, res) => {
   try {
-    const users = await User.find({}, { name: 1, email: 1, password: 1, score: 1, _id: 0 });
+    const users = await User.find(
+      {},
+      { name: 1, email: 1, password: 1, score: 1, _id: 0 }
+    );
 
     const numberedUsers = users.map((user, index) => ({
       number: index + 1,
@@ -105,8 +106,8 @@ app.get('/users', async (req, res) => {
 
     res.status(200).json(numberedUsers);
   } catch (error) {
-    console.error('Error fetching users:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error("Error fetching users:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
