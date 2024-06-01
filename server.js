25c6926d3aa1598702a27b38fe6de8c5733bcdfe
olayinkagrace <olayinka4grace@gmail.com>
2024-06-01 09:40:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  score: { type: Number, default: null },
+  score: { type: Number, default: 0 },
 });
 
 const User = mongoose.model('User', userSchema);
