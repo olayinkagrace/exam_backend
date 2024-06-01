8610dd0a7571423d15750803966e80cbc1cf53d4
olayinkagrace <olayinka4grace@gmail.com>
2024-06-01 21:13:57 +0100

    commit

--- server.js
+++ server.js
@@ -111,4 +111,20 @@ app.get("/users", async (req, res) => {
   }
 });
 
+// Add this route after the "/users" route
+app.delete("/userDelete/:id", async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+
 const PORT = process.env.PORT || 5000;
